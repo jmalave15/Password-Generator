6f8a73c331565e173b0acee891e42632e6b61065
jmalave15 <qmalavejose1@gmail.com>
2023-06-05 00:02:36 -0400

    made code cleaner and found bug

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,12 +1,8 @@
-
 //number, character and letters
-
 var upperCase = "ABCDEFGHIJKLMONPQRSTUVWXYZ";
 var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 var number = "0123456789";
 var special = "~!#$%^&*()+=";
-
-var completePassword;
 
 //Event listener for generating password (reference)
 var generateBtn = document.querySelector("#generate");
@@ -15,55 +11,52 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+
+  passwordText.innerHTML = password;
 }
 
 generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
-  var all= "";
+  var all = "";
 
-  var length = parseINT(window.prompt("How many letter do you want choose between 8 and 128"));
+  var length = parseInt(window.prompt("How many letters do you want? Choose a number between 8 and 128"));
 
   if (length < 8 || length > 128) {
-    alert("Error:Please enter a number 8-128 characters"); 
+    alert("Error: Please enter a number between 8 and 128 characters");
     var input = window.prompt("Please enter a number between 8 and 128 characters");
     length = parseInt(input);
     if (length < 8 || length > 128) {
-      return alert("Error:Please enter a number between 8 and 128 characters");
+      return "Error: Please enter a number between 8 and 128 characters";
     }
-}
+  }
 
-   //to see if lower case is needed
+  //to see if lower case is needed
   if (window.confirm("Do you need lowercase letters?")) {
-  all += lowerCase;
+    all += lowerCase;
+  }
 
-}
+  //to see if upper case is needed
+  if (window.confirm("Do you need uppercase letters in the password?")) {
+    all += upperCase;
+  }
 
-//to see if upper case is needed
-  if(window.confirm("Do you need uppercase letters in password?")) {
-  all += upperCase;
+  //To see if numbers are needed
+  if (window.confirm("Do you need numbers in the password?")) {
+    all += number;
+  }
 
-}
+  //To see if special characters are wanted
+  if (window.confirm("Do you need special characters in the password?")) {
+    all += special;
+  }
 
-//To see if numbers are needed
-if(window.confirm("Do you need numbers in password?")) {
-  all+= number;
+  // Loop that runs for the length of each statement
+  var allChars = "";
+  for (var i = 0; i < length; i++) {
+    allChars += all.charAt(Math.floor(Math.random() * all.length));
+  }
 
-}
-
-//To see if special characters are wanted
-if(window.confirm("Do you need special characters in password?")) {
-  all+= special;
-
-}
-
-// Loop that runs for the length of each statement
-var allPass = "";
-for (var i=0; i < length; i++) {
-  allPass += all.charAt(Math.floor(Math.random() * all.length));  
-
-}
-
-//password generated returns here
-return allPass;
+  //password generated returns here
+  return allChars;
 }
